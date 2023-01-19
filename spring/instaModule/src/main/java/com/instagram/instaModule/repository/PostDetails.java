9060e52d7f32ab2ecd20eee367207d154fc15d0a
Kavimathi <kavimathi26.10.01rathinasamy@gmail.com>
2023-01-19 12:25:52 +0530

    modified the post count in create and delete post method

--- spring/instaModule/src/main/java/com/instagram/instaModule/repository/PostDetails.java
+++ spring/instaModule/src/main/java/com/instagram/instaModule/repository/PostDetails.java
@@ -4,6 +4,7 @@ import com.instagram.instaModule.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -11,10 +12,7 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Repository
 public class PostDetails {
@@ -23,17 +21,23 @@ public class PostDetails {
     public List<Post> getPostsByProfileId(String profileId) {
         return mongoTemplate.find(Query.query(Criteria.where("profileId").is(profileId)),Post.class);
     }
+
+    public long countOfPosts(String profileId) {
+        User user = new User();
+        Post post = new Post();
+        post.setProfileId(profileId);
+        user.setPostCount(getPostsByProfileId(profileId).size());
+        System.out.println("count of post : "+user.getPostCount());
+        return user.getPostCount();
+    }
     public Post createPost(String profileId,String posts) { //add-post
     Post post = new Post();
-    User user = new User();
     post.setProfileId(profileId);
     post.setPosts(posts);
     post.setPostUploadedTime(System.currentTimeMillis());
     System.out.println(post.getPostUploadedTime());
     mongoTemplate.save(post);
-    user.setPostCount(getPostsByProfileId(profileId).stream().count());
-//    user.setPostCount(getPostsByProfileId(profileId).size());
-    System.out.println(user.getPostCount());
+    countOfPosts(profileId);
     return post;
     }
     public List<Post> findAll() {
@@ -41,10 +45,14 @@ public class PostDetails {
         return mongoTemplate.find(query, Post.class);
     }
     public void deletePost(String postId) {
+        String profileId = mongoTemplate.findById(postId,Post.class).getProfileId();
         mongoTemplate.remove(Query.query(Criteria.where("postId").is(postId)), Post.class);
+        System.out.println("after deletion: "+profileId);
+        countOfPosts(profileId);
     }
     public List getTopPosts() {
         Query query = new Query();
+        query.limit(5);
         query.with(Sort.by(Sort.Direction.DESC, "postUploadedTime"));
         return mongoTemplate.find(query,Post.class);
     }
